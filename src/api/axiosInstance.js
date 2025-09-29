33dd9ccfde1d10e455090a31b099caa547d8f2c5
Agolomanyaa <t-a-n-4-8@hotmail.com>
2025-09-29 12:45:47 +0300

    Son Düzenlemeler

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8081/api/v1'; // ESKİ ADRESİ BUNUNLA DEĞİŞTİR
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL + '/api/v1';
 
 // Axios instance oluştur
 const axiosInstance = axios.create({
